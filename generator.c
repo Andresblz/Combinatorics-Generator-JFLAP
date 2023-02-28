2f1f83b7af9002e6c42789189a63927615b7ccd6
Andresblz <andresblz@usal.es>
2023-02-28 09:45:26 +0100

    :sparkles: Added initial variables

--- generator.c
+++ generator.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX 1000
 
-int main(int argc, char *argv[]) {
+int main(int argc, const char *argv[]) {
+    int wordCount;
+    char alphabet[MAX];
 
-    if (argc < 2) {
-        fprintf(stderr, "Use: generator [alphabet] [alphabet]");
+    if (argc <= 1) {
+        fprintf(stderr, "Use: generator (alphabet) [word count]");
+        exit(1);
     }
 
+    strcpy(alphabet, argv[1]);
+
+    if (argc == 3) {
+        wordCount = atoi(argv[2]);
+    }
+
+    puts("");
     return 0;
 }
