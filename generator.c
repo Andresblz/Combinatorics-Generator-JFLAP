6e2d6989aecabd98752958f9cff6cf6b5f7d4f98
Andresblz <andresblz@usal.es>
2023-02-28 09:16:37 +0100

    :tada: Added generator.c

---
+++ generator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 1000
+
+int main(int argc, char *argv[]) {
+
+    if (argc < 2) {
+        fprintf(stderr, "Use: generator [alphabet] [alphabet]");
+    }
+
+    return 0;
+}
